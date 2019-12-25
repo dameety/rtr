14be0edba6b03c0e2c2de8afb2d2f20ec509ee14
dameety <djdamilola01@gmail.com>
2019-12-25 21:18:52 +0100

    reduce meal units during order checkout

--- app/Actions/CreateOrderAction.php
+++ app/Actions/CreateOrderAction.php
@@ -1,23 +1,32 @@
 <?php
 
-
 namespace App\Actions;
 
-
+use App\Meal;
 use App\Order;
 
 class CreateOrderAction
 {
     public function call($data)
     {
-
-        return Order::create([
+        $order = Order::create([
             'cart' => json_encode($data['cart']),
             'user_id' => auth()->user()->id,
             'address' => $data['address'],
             'total_amount' => $data['total_amount']
         ]);
 
+        $this->reduceMealUnits($data['cart']);
 
+        return $order;
+    }
+
+    private function reduceMealUnits($cart)
+    {
+        collect($cart)->each(function ($item, $key) {
+            $meal= Meal::findOrFail($item['meal_id']);
+
+            $meal->reduceUnits();
+        });
     }
 }
