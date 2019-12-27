77ce99c49935c02e316ea00d30515ff778ecc09f
dameety <djdamilola01@gmail.com>
2019-12-27 19:44:05 +0100

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Meal;
 use App\Observers\MealObserver;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceSchema('https');
+        }
         Meal::observe(MealObserver::class);
     }
 }
